4cd2e4ecc1720c8e8914e0f3c7098cabd2a963e9
sssylvain <club@zupton.com>
2022-06-15 15:49:33 -0400

    commentaires ajoutes

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ namespace ConsoleApp1
         {
             do
             {
-                Console.WriteLine("Inserez la lettre :");
+                //lusager doit enter une lettre afin dobtenir une note musicale qui est attribuee a la valeur de la lettre//
+                Console.WriteLine("Inserez une lettre entre A et G elle sera convertie en note musicale :");
                 String Note = Console.ReadLine().ToUpper();
 
                 if (Note == "A")
@@ -42,8 +43,10 @@ namespace ConsoleApp1
                 }
                 else
                 {
-                    Console.WriteLine("La note choisi n'existe pas. Essayer encore");
+                    //messagederreur//
+                    Console.WriteLine("La note choisie n'existe pas. Essayer encore");
                 }
+                //demande si lusager veut continuer de jouer//
                 Console.WriteLine("Avez vous une autre lettre a convertir ? \n O = Oui \n N = Non");
             } while (Console.ReadLine().ToUpper() == "O");
             Console.WriteLine("Bye bye");
